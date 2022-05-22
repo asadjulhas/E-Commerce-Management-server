77c18cc388d9005059b8d45c0669437620530bfd
Asadujjaman Julhas <fahim0760@gmail.com>
2022-05-23 00:39:38 +0600

    Create route for load single product

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 
 
 const port = process.env.PORT || 5000;
@@ -12,6 +13,23 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // Middleware
 app.use(cors());
 app.use(express.json())
+
+// Verify user token
+function VerifyUser (req, res, next)  {
+  const accessToken = req.headers.authorization;
+  if(!accessToken){
+    return res.status(401).send({Message: 'unauthorized access'})
+  }
+  const token = accessToken.split(' ')[1];
+  jwt.verify(token, process.env.JWT, function(err, decoded) {
+    if(err) {
+      return res.status(403).send({Message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mo3lp.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,11 +42,27 @@ async function run() {
 
     const productsCollections = client.db('borak').collection('products')
 
+    // Create JWT
+app.post('/login', async (req, res) => {
+  const user = req.body;
+  const accessToken = jwt.sign(user, process.env.JWT, {
+    expiresIn: '1d'
+  })
+  res.send({accessToken})
+})
 
     // Get all products
     app.get('/product', async (req, res) => {
       const products = await productsCollections.find().toArray();
       res.send(products)
+    })
+
+    // Get single products
+    app.get('/product/:id', VerifyUser, async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const tool = await productsCollections.findOne(query);
+      res.send(tool)
     })
 
   }
