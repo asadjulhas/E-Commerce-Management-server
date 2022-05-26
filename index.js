3b813d907e7c1c447d99e62fdfaabfceab5c7236
Asadujjaman Julhas <fahim0760@gmail.com>
2022-05-26 12:16:42 +0600

    Implement product update option for admin

--- index.js
+++ index.js
@@ -48,17 +48,17 @@ async function run() {
     const paymentCollections = client.db("borak").collection("payments");
     const reviewCollections = client.db("borak").collection("reviews");
 
-        // Verify admin
-        const verifyAdmin = async (req, res, next) => {
-          const tokenEmail = req.decoded.email;
-            const tokenQuery = {email: tokenEmail, role: 'admin'}
-            const checkAdmin = await userCollections.findOne(tokenQuery);
-            if(checkAdmin) { 
-              next();
-            } else {
-              res.send({message: 'You dont have admin access bro'})
-            }
-          }
+    // Verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const tokenEmail = req.decoded.email;
+      const tokenQuery = { email: tokenEmail, role: "admin" };
+      const checkAdmin = await userCollections.findOne(tokenQuery);
+      if (checkAdmin) {
+        next();
+      } else {
+        res.send({ message: "You dont have admin access bro" });
+      }
+    };
 
     // Payment function
 
@@ -182,93 +182,126 @@ async function run() {
       res.send(result);
     });
 
-// Update user profile
-app.post("/update-profile", VerifyUser, async (req, res) => {
-  const data = req.body; 
-  const query = {email: data.email};
-  const updateDoc = {
-    $set: {phone: data.phone, address: data.address, linkedin: data.linkedin}
-  };
-  const result = await userCollections.updateOne(query, updateDoc);
-  res.send(result);
-});
+    // Update user profile
+    app.post("/update-profile", VerifyUser, async (req, res) => {
+      const data = req.body;
+      const query = { email: data.email };
+      const updateDoc = {
+        $set: {
+          phone: data.phone,
+          address: data.address,
+          linkedin: data.linkedin,
+        },
+      };
+      const result = await userCollections.updateOne(query, updateDoc);
+      res.send(result);
+    });
 
+    // Get a user
+    app.get("/user/:email", VerifyUser, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollections.findOne(query);
+      res.send(result);
+    });
 
- // Get a user
- app.get("/user/:email", VerifyUser, async (req, res) => {
-  const email = req.params.email;
-  const query = {email: email};
-  const result = await userCollections.findOne(query);
-  res.send(result);
-});
+    // Check admin
+    app.get("/check-admin/:email", VerifyUser, async (req, res) => {
+      const email = req.params.email;
+      const Query = { email: email, role: "admin" };
+      const checkAdmin = await userCollections.findOne(Query);
+      res.send(checkAdmin);
+    });
 
+    // Get all user
+    app.get("/all-users", VerifyUser, verifyAdmin, async (req, res) => {
+      const query = {};
+      const allUsers = await userCollections.find(query).toArray();
+      res.send(allUsers);
+    });
 
-// Check admin
-app.get('/check-admin/:email', VerifyUser, async (req, res) => {
-  const email = req.params.email;
-  const Query = {email: email, role: 'admin'}
-  const checkAdmin = await userCollections.findOne(Query);
-  res.send(checkAdmin)
-})
-
- // Get all user
- app.get('/all-users', VerifyUser, verifyAdmin, async (req, res) => {
-  const query = {};
-  const allUsers = await userCollections.find(query).toArray();
-  res.send(allUsers);
-})
-
-  // Remove Admin 
-  app.put('/remove-admin/:email', VerifyUser, verifyAdmin, async (req, res) => {
-    const email = req.params.email;
-    const query = {email: email};
-    const updateDoc = {
-      $set: {role: ''}
-    };
-    const result = await userCollections.updateOne(query, updateDoc);
-    res.send({result})
-  })
-
-      // Make Admin 
-      app.put('/admin/:email', VerifyUser, verifyAdmin, async (req, res) => {
+    // Remove Admin
+    app.put(
+      "/remove-admin/:email",
+      VerifyUser,
+      verifyAdmin,
+      async (req, res) => {
         const email = req.params.email;
-          const query = {email: email};
+        const query = { email: email };
         const updateDoc = {
-          $set: {role: 'admin'}
+          $set: { role: "" },
         };
         const result = await userCollections.updateOne(query, updateDoc);
-        res.send({result})
-        
-      })
+        res.send({ result });
+      }
+    );
 
-        // Add Product
-    app.post('/product', VerifyUser, verifyAdmin, async (req, res) => {
+    // Make Admin
+    app.put("/admin/:email", VerifyUser, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollections.updateOne(query, updateDoc);
+      res.send({ result });
+    });
+
+    // Add Product
+    app.post("/product", VerifyUser, verifyAdmin, async (req, res) => {
       const data = req.body;
-       const result = await productsCollections.insertOne(data);
-       res.send(result)
-    })
+      const result = await productsCollections.insertOne(data);
+      res.send(result);
+    });
 
-       // Get all orders
-       app.get("/orders", VerifyUser, verifyAdmin, async (req, res) => {
-          const query = {};
-          const orders = await orderCollections.find(query).toArray();
-          res.send(orders);
-      });
+    // Get all orders
+    app.get("/orders", VerifyUser, verifyAdmin, async (req, res) => {
+      const query = {};
+      const orders = await orderCollections.find(query).toArray();
+      res.send(orders);
+    });
 
-
-    // Update status for shipped 
+    // Update status for shipped
     app.put("/status/:id", VerifyUser, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const updateDoc = {
-        $set: { status: 'shipped'},
+        $set: { status: "shipped" },
       };
       const result = await orderCollections.updateOne(query, updateDoc);
       res.send(result);
     });
 
+    // Delete order
+    app.delete("/product/:id", VerifyUser, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    // Make Admin
+    app.put("/product", VerifyUser, verifyAdmin, async (req, res) => {
+      const data = req.body;
+      const id = data.id;
+      const query = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: data.fname,
+          description: data.fdescription,
+          minOrder: data.fminOrder,
+          price: data.fprice,
+          stock: data.fstock,
+          type: data.ftype,
+        },
+      };
+      // console.log(updateDoc)
+      const result = await productsCollections.updateOne(query, updateDoc);
+      res.send({result})
+    });
 
 
+    
   } finally {
     // await client.close()
   }
