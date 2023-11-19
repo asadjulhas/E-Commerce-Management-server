b2f62708801189d352a2db345bac9d184d89ec4f
asadjulhas <fahim0760@gmail.com>
2023-11-20 01:15:36 +0600

    Change port to 5000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 var jwt = require("jsonwebtoken");
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 // For Payment
 const stripe = require("stripe")(process.env.STRIPE_SECRATE_KEY);
